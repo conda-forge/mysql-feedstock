2ad9b7170fed1ada5368c25615f9b380010043d4
Mark Harfouche <mark.harfouche@gmail.com>
2021-01-05 12:11:16 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 set -x
 
+_rpcgen_hack_dir=""
+if [[ "${target_platform}" == *"linux"* ]]; then
+    _rpcgen_hack_dir=$SRC_DIR/rpcgen_hack
+    _target_sysroot=$($CXX --print-sysroot)
+    _target_rpcgen_bin=${_target_sysroot}/usr/bin/rpcgen
+    _target_interpreter=${_target_sysroot}/$(patchelf --print-interpreter ${_target_rpcgen_bin})
+    _target_libdir=${_target_sysroot}/$(dirname ${_target_interpreter})
+    mkdir -p $_rpcgen_hack_dir/bin
+    cat <<EOF > ${_rpcgen_hack_dir}/bin/rpcgen
+#!/bin/bash
+${_target_interpreter} --library-path ${_target_libdir} ${_target_rpcgen_bin} -Y ${_rpcgen_hack_dir}/bin \$@
+EOF
+    ln -s $(readlink -f ${CPP}) ${_rpcgen_hack_dir}/bin/cpp
+    chmod +x ${_rpcgen_hack_dir}/bin/{rpcgen,cpp}
+fi
+
 declare -a _xtra_cmake_args
 if [[ $target_platform == osx-64 ]]; then
     _xtra_cmake_args+=(-DWITH_ROUTER=OFF)
@@ -11,7 +27,7 @@ export CXXFLAGS="${CXXFLAGS:-} -fno-pie"
 cmake -S$SRC_DIR -Bbuild -GNinja \
   -DCMAKE_CXX_STANDARD=14 \
   -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_PREFIX_PATH=$PREFIX \
+  -DCMAKE_PREFIX_PATH="${_rpcgen_hack_dir};$PREFIX" \ 
   -DCOMPILATION_COMMENT=conda-forge \
   -DCMAKE_FIND_FRAMEWORK=LAST \
   -DWITH_UNIT_TESTS=OFF \
@@ -36,7 +52,5 @@ cmake -S$SRC_DIR -Bbuild -GNinja \
   -DINSTALL_SUPPORTFILESDIR=mysql/support-files \
   "${_xtra_cmake_args[@]}"
 
-# PPC64le fails with too many jobs
-MAX_JOBS=4
-CPU_COUNT=$(( CPU_COUNT < MAX_JOBS ? CPU_COUNT : MAX_JOBS ))
-cmake --build build --parallel ${CPU_COUNT}
+# PPC64le fails with too many jobs????
+cmake --build build --parallel 1
